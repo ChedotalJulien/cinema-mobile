b62009136883a0f0fdca93aa8260907e1b130ad1
seydou <baseydou95@gmail.com>
2020-09-05 23:35:54 +0000

    page accueil villes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:cinema_mobile_app/menuItem.dart';
 import 'package:cinema_mobile_app/setting-page.dart';
 import 'package:cinema_mobile_app/villes-page.dart';
 import 'package:flutter/material.dart';
+import 'package:cinema_mobile_app/app-util.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import './cinemas-page.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
@@ -22,6 +26,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<dynamic> listVilles;
 
   final menus = [
     {'title': 'Home', 'icon': Icon (Icons.home), 'page': VillePage()},
@@ -37,8 +42,35 @@ class _MyAppState extends State<MyApp> {
         title: Text("Cinema Page "),
       ),
       body: Center(
-        child: Text("Home Cinema ....."),
-      ),
+          child: this.listVilles==null?CircularProgressIndicator():
+            ListView.builder(
+                itemCount: (this.listVilles==null)?0:this.listVilles.length,
+                itemBuilder : (context,index){
+                  return Card(
+                    color: Colors.orange,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        child: Text(this.listVilles[index]['name'],
+                          style: TextStyle(
+                              color: Colors.black
+                          ),
+
+                        ),
+                        onPressed: (){
+                          Navigator.push(context, MaterialPageRoute(
+                              builder: (context)=> new CinemasPage(this.listVilles[index])
+                          ));
+                        },
+                      ),
+                    ),
+                  );
+                }
+
+            )
+        ),
+
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
@@ -72,4 +104,28 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadVilles();
+  }
+
+  void loadVilles() {
+    //  String url = "http://192.168.1.5:8080/villes";
+    String url = AppUtil.host+ "/villes";
+    http.get(url)
+        .then((resp){
+      setState(() {
+        this.listVilles= json.decode(resp.body)['_embedded']['villes'];
+      });
+
+
+    }).catchError((err){
+      print(err);
+    })
+    ;
+  }
 }
+
